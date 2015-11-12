6c6adb6613e991dc9c5bdab86ac83f0f229b4f93
Chang Liu <cliu@groupon.com>
2015-11-12 16:39:57 -0600

    Initial Commit

---
+++ canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  canvas
+//
+//  Created by Chang Liu on 11/12/15.
+//  Copyright © 2015 Chang Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ canvas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  canvas
+//
+//  Created by Chang Liu on 11/12/15.
+//  Copyright © 2015 Chang Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
